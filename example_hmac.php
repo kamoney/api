de80e03d4f8ecefa380847e9c5012eef2be001c9
Claudeci Goularte <claudeci@kamoney.com.br>
2018-11-10 21:51:56 -0200

    Update example_hmac.php
    Exemplo de solicitação, para API Kamoney

--- example_hmac.php
+++ example_hmac.php
@@ -1,24 +1,31 @@
-$mt = explode(' ', microtime());
-$req['nonce'] = $mt[1].substr($mt[0], 2, 6);
+<?php
+# EXAMPLE POST REQUEST
 
-# Posts or Gets
+$YOUR_PUBLIC_KEY = "YOUR PUBLIC KEY HERE";
+$YOUR_SECRET_KEY = "YOUR SECRET KEY HERE";
+
+# NONCE DATA
+$mt = explode(' ', microtime());
+$req['nonce'] = $mt[1].substr($mt[0], 2, 6); // OBRIGATÓRIO
+
+# POSTS OR GETS TO ENDPOINT
 foreach ($_POST as $key => $value)
 {
     $req[$key] = $value;
 }
 
-# Generates query string in URL format
+# GENERATES QUERY STRING IN URL FORMAT
 $data = http_build_query($req, '', '&');
 
-# add headers
+# ADD HEADERS PUBLIC AND SIGN
 $headers = array(
-    'public: ' . YOUR_PUBLIC_KEY,
-    'sign: ' . hash_hmac('sha512', $data, YOUR_SECRET_KEY),
+    'public: ' . $YOUR_PUBLIC_KEY,
+    'sign: ' . hash_hmac('sha512', $data, $YOUR_SECRET_KEY),
 );
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, "URL REQUEST HERE");
+curl_setopt($ch, CURLOPT_URL, "URL_API_REQUEST_HERE");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
@@ -27,3 +34,4 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 $data = curl_exec($ch);
 
 return $data;
+?>
